52e9a0227f3e23e629b0936ad631efe4f59f4e13
tsukaryu <drgon8otsuka@gmail.com>
2021-01-23 22:10:07 +0900

    finish 105

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 
+import Aux from "../../../hoc/Aux";
 import classes from "./Person.module.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
     );
   }
 }
